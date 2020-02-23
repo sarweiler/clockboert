696dfa67b1628e5fe36922d1159818a93dfa969e
Sven Arweiler <dev@sba.io>
2020-02-24 00:37:21 +0100

    fixes. a lot.

--- clockr.ino
+++ clockr.ino
@@ -1,31 +1,49 @@
-int led = 12;
+int gateLed = 0;
+int urzwergClockLed = 1;
+int urzwergStartLed = 2;
+int glitchLed = 3;
+
 int gateOut1 = 14;
+int gateOutPO = 12;
+int gateOut2x = 11;
+int gateOutLsdj = 13;
 int urzwergOut = 15;
 int glitchOut = 16;
+
 int glitchIn = 8;
 int startIn = 9;
 int stopIn = 10;
+int bpmIn = A0;
 
 int bpm = 160;
 
-int clockTimer = 0;
-int urzwergTimer = 0;
-int glitchTimer = 0;
+long clockTimer = 0;
+long urzwergTimer = 0;
+long glitchTimer = 0;
 
-int gateTimer = 0;
-int urzwergGateTimer = 0;
+long gateTimer = 0;
+long urzwergGateTimer = 0;
 
 int pulseWidth = 1;
 int urzwergPulseWidth = 1;
+int glitchLedLength = 25;
+int urzwergLedLength = 10;
+int gateLedLength = 25;
 
-int buttonDebouncer = 0;
+long buttonDebouncer = 0;
 
 int glitchIntervalDivider = 1;
 float glitchIntervalDividers[] = {0.25, 0.5, 4.0, 8.0, 16.0};
 
 boolean gateHigh = false;
+boolean gate2xHigh = false;
 boolean glitchGateHigh = false;
 boolean urzwergGateHigh = false;
+boolean poGateHigh = false;
+
+boolean glitchLedOn = false;
+boolean urzwergLedOn = false;
+boolean gateLedOn = false;
 
 boolean glitchActive = false;
 boolean running = false;
@@ -33,18 +51,32 @@ boolean running = false;
 void setup() {
   Serial.begin(38400);
   
-  pinMode(led, OUTPUT);
+  pinMode(gateLed, OUTPUT);
+  pinMode(urzwergClockLed, OUTPUT);
+  pinMode(urzwergStartLed, OUTPUT);
+  pinMode(glitchLed, OUTPUT);
+  
   pinMode(gateOut1, OUTPUT);
   pinMode(urzwergOut, OUTPUT);
   pinMode(glitchOut, OUTPUT);
+  pinMode(gateOutPO, OUTPUT);
+  pinMode(gateOut2x, OUTPUT);
+  pinMode(gateOutLsdj, OUTPUT);
+  
   pinMode(startIn, INPUT);
   pinMode(stopIn, INPUT);
   pinMode(glitchIn, INPUT);
 
-  digitalWrite(led, LOW);
+  digitalWrite(gateLed, LOW);
+  digitalWrite(urzwergClockLed, LOW);
+  digitalWrite(urzwergStartLed, LOW);
+  digitalWrite(glitchLed, LOW);
   digitalWrite(gateOut1, LOW);
   digitalWrite(urzwergOut, LOW);
   digitalWrite(glitchOut, LOW);
+  digitalWrite(gateOutPO, LOW);
+  digitalWrite(gateOut2x, LOW);
+  digitalWrite(gateOutLsdj, LOW);
   
   resetAnalogTimers();
   buttonDebouncer = millis();
@@ -91,76 +123,152 @@ int bpmToMillis24Ppqn(int bpm) {
   return round(bpmToMillis(bpm) / 24);
 }
 
+void allLedsOff() {
+  digitalWrite(gateLed, LOW);
+  digitalWrite(glitchLed, LOW);
+  digitalWrite(urzwergStartLed, LOW);
+  digitalWrite(urzwergClockLed, LOW);
+}
+
+
+// ANALOG GATE
 void setAnalogGateHigh() {
-  digitalWrite(led, HIGH);
+  digitalWrite(gateLed, HIGH);
   digitalWrite(gateOut1, HIGH);
+  setLsdjGateHigh();
+  digitalWrite(glitchLed, HIGH);
   resetAnalogTimers();
   gateHigh = true;
+  gateLedOn = true;
 }
 
+void setAnalogGateLow() {
+  digitalWrite(gateOut1, LOW);
+  setLsdjGateLow();
+  resetGateTimer();
+  gateHigh = false;
+}
+
+void setLsdjGateHigh() {
+  Serial.println("LSDJ HIGH");
+  digitalWrite(gateOutLsdj, HIGH);
+}
+
+void setLsdjGateLow() {
+  Serial.println("LSDJ LOW");
+  digitalWrite(gateOutLsdj, LOW);
+}
+
+// 2X GATE
+void set2xGateHigh() {
+  digitalWrite(gateOut2x, HIGH);
+  gate2xHigh = true;
+}
+
+void set2xGateLow() {
+  digitalWrite(gateOut2x, LOW);
+  gate2xHigh = false;
+}
+
+// GLITCH GATE
 void setGlitchGateHigh() {
   digitalWrite(glitchOut, HIGH);
+  digitalWrite(glitchLed, HIGH);
   resetGlitchTimer();
   glitchGateHigh = true;
+  glitchLedOn = true;
 }
 
+void setGlitchGateLow() {
+  digitalWrite(glitchOut, LOW);
+  resetGlitchTimer();
+  glitchGateHigh = false;
+}
+
+
+// URZWERG GATE
 void setUrzwergGateHigh() {
   resetUrzwergTimer();
   digitalWrite(urzwergOut, HIGH);
+  digitalWrite(urzwergClockLed, HIGH);
   urzwergGateHigh = true;
+  urzwergLedOn = true;
 }
 
 void setUrzwergGateLow() {
   resetUrzwergGateTimer();
   digitalWrite(urzwergOut, LOW);
+  digitalWrite(urzwergStartLed, LOW);
   urzwergGateHigh = false;
 }
 
+
+// PO
+void swapPoGate() {
+  if(poGateHigh) {
+    Serial.println("PO high");
+    digitalWrite(gateOutPO, HIGH);
+  } else {
+    Serial.println("PO low");
+    digitalWrite(gateOutPO, LOW);
+  }
+  poGateHigh = !poGateHigh;
+}
+
+
+// LOOP
 void loop() {
-  int bpmRead = round(analogRead(0) / 5);
+  int bpmRead = round(analogRead(bpmIn) / 5);
   bpmRead = bpmRead >= 10 ? bpmRead : 10;
   int intervalMillis = bpmToMillis(bpmRead) / 2;
   
-  int now = millis();
-  int urzwergInterval = now - urzwergTimer;
-  int analogInterval = now - clockTimer;
-  int glitchInterval = now - glitchTimer;
+  long now = millis();
+  long urzwergInterval = now - urzwergTimer;
+  long analogInterval = now - clockTimer;
+  long glitchInterval = now - glitchTimer;
 
-  // Start/Stop button
+
+  // START/STOP BUTTON
   if(digitalRead(startIn) == HIGH) {
-    //Serial.print("BUTTON: ");
-    //Serial.println(now - buttonDebouncer);
     if((now - buttonDebouncer) >= 1000) {
       if(!running) {
         Serial.println("START");
         // Weird Urzwerg needs a delay and
         // an extra impulse for syncing correctly
+        digitalWrite(urzwergStartLed, HIGH);
         delay(100);
         setUrzwergGateHigh(); 
         resetAnalogTimers();
         running = true;
       } else {
+        digitalWrite(urzwergStartLed, HIGH);
+        delay(100);
+        digitalWrite(urzwergStartLed, LOW);
         Serial.println("STOP");
         resetAnalogTimers();
         running = false;
+        allLedsOff();
       }
       buttonDebouncer = millis();
     }
   }
 
-  // Stop button
+
+  // STOP BUTTON
   if(digitalRead(stopIn) == HIGH) {
-    if((now - buttonDebouncer) >= 1000) {
+    if((now - buttonDebouncer) >= 100) {
       Serial.println("STOP");
       resetAnalogTimers();
       running = false;
       buttonDebouncer = millis();
+      allLedsOff();
     }
   }
 
-  // Glitch button
+
+  // GLITCH BUTTON
   if(digitalRead(glitchIn) == HIGH) {
-    if((now - buttonDebouncer) >= 1000) {
+    if((now - buttonDebouncer) >= 100) {
       Serial.println("GLITCH");
       glitchActive = true;
       buttonDebouncer = millis();
@@ -171,30 +279,41 @@ void loop() {
   }
 
 
-  // Clock output
+  // CLOCK
   if(running) {
     // Analog clock
     if(analogInterval >= (intervalMillis)) {
-      Serial.print("bpm: ");
-      Serial.print(bpmRead);
-      Serial.print(" / ms: ");
-      Serial.print(intervalMillis);
-      Serial.print(" / r: ");
-      Serial.println(bpmRead);
+      //Serial.print("bpm: ");
+      //Serial.print(bpmRead);
+      //Serial.print(" / ms: ");
+      //Serial.print(intervalMillis);
+      //Serial.print(" / r: ");
+      //Serial.println(bpmRead);
 
       if(glitchActive) {
         glitchIntervalDivider = round(glitchIntervalDividers[random(5)]);
       }
       setAnalogGateHigh();
       setGlitchGateHigh();
-      setUrzwergGateHigh();
+      swapPoGate();
     }
   
     if(gateHigh && ((now - urzwergPulseWidth  - gateTimer) >= pulseWidth)) {
-      digitalWrite(led, LOW);
-      digitalWrite(gateOut1, LOW);
-      resetGateTimer();
-      gateHigh = false;
+      setAnalogGateLow();
+    }
+
+    if(gateLedOn && ((now - urzwergPulseWidth  - gateTimer) >= gateLedLength)) {
+      digitalWrite(gateLed, LOW);
+      gateLedOn = false;
+    }
+
+    // 2x clock
+    if(analogInterval >= (intervalMillis / 2)) {
+      set2xGateHigh();
+    }
+
+    if(gate2xHigh && ((now - gateTimer) >= pulseWidth)) {
+      set2xGateLow();
     }
 
     // Glitch clock
@@ -203,9 +322,12 @@ void loop() {
     }
 
     if(glitchGateHigh && ((now - glitchTimer) >= pulseWidth)) {
-      digitalWrite(glitchOut, LOW);
-      resetGlitchTimer();
-      glitchGateHigh = false;
+      setGlitchGateLow();
+    }
+
+    if(glitchLedOn && ((now - glitchTimer) >= glitchLedLength)) {
+      digitalWrite(glitchLed, LOW);
+      glitchLedOn = false;
     }
   
     // Urzwerg clock
@@ -215,6 +337,11 @@ void loop() {
   
     if(urzwergGateHigh && ((now - urzwergGateTimer) >= urzwergPulseWidth)) {
       setUrzwergGateLow();
+    }
+
+    if(urzwergLedOn && ((now - urzwergGateTimer) >= urzwergLedLength)) {
+      digitalWrite(urzwergClockLed, LOW);
+      urzwergLedOn = false;
     }
   }
 }
